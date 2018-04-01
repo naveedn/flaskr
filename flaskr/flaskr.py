3bb635bcaa7ef3dbe3196010cb92a52b1f7968a1
Naveed Nadjmabadi <naveed.nadjmabadi@adp.com>
2018-04-01 15:04:19 -0400

    Add db initialization step

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -24,3 +24,27 @@ def connect_db():
     rv.row_factory = sqlite3.Row
     return rv
 
+def init_db():
+  db = get_db()
+  with app.open_resource('schema.sql', mode='r') as f:
+    db.cursor().executescript(f.read())
+  db.commit()
+
+@app.cli.command('initdb')
+def initdb_command():
+  """Initializes the database"""
+  init_db()
+  print('Initiailized the database')
+
+def get_db():
+  """singleton pattern for handling db connections"""
+  if not hasattr(g, 'sqlite_db'):
+    g.sqlite_db = connect_db()
+  return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+  """Closes the db at the end of a request"""
+  if hasattr(g, 'sqlite_db'):
+    g.sqlite_db.close()
+
